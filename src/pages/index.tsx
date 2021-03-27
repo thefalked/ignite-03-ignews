681519c3c2e857149b89a4b4ce9527349aee0e24
thefalked <giulainocrivelli@hotmail.com>
2021-03-26 20:03:57 -0400

    created and styled subscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
 
@@ -27,7 +28,7 @@ export default function Home() {
             <span>for $9.90 month</span> 
           </p>
 
-
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding"/>
