36014eb719a499b57f5c7a3e554ddafcb2addd91
thefalked <giulainocrivelli@hotmail.com>
2021-03-31 23:01:58 -0400

    listing posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,24 @@
 import { GetStaticProps } from "next";
 import Head from "next/head";
 import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
 import { getPrismicClient } from "../../services/prismic";
 
 import styles from "./styles.module.scss";
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -17,18 +29,19 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="">
-            <time>
-              12 de março de 2021
-            </time>
-            <strong>
-              Creating a monorepo with lear & code
-            </strong>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex inventore quaerat fugit, 
-              quo velit tempora natus atque eum aliquid nihil?
-            </p>
-          </a>
+          { posts.map(post => (
+            <a href="" key={post.slug}>
+              <time>
+                {post.updatedAt}
+              </time>
+              <strong>
+                {post.title}
+              </strong>
+              <p>
+                {post.excerpt}
+              </p>
+            </a>
+          )) }
         </div>
       </main>
     </>
@@ -45,10 +58,22 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(response);
-  
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === "paragraph")?.text ?? "",
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      })
+    }
+  });
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
