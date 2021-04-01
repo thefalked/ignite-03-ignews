bb175f4338a1204665fac83ca5895b8d6902cd13
thefalked <giulainocrivelli@hotmail.com>
2021-04-01 00:33:34 -0400

    validating user subscription

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from 'next-auth/client';
+import { useRouter } from 'next/dist/client/router';
+
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 
@@ -10,11 +12,17 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession();
+  const router = useRouter()
 
   async function handleSubscribe() {
     if (!session) {
       signIn("github")
 
+      return;
+    }
+
+    if (session.activeSubscription) {
+      router.push("/posts");
       return;
     }
 
